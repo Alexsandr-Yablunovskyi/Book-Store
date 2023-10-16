400b09236bc034d33f25d3772bc074ae17a12cf2
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-10-16 13:53:04 +0300

    Initial commit

---
+++ src/test/java/yablunovskyi/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package yablunovskyi.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
