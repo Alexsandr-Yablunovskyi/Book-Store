43673c4050365dca628e91e61837587033a42485
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-10-19 13:11:02 +0300

    Checkstyle "record" problem is solved

--- src/main/java/yablunovskyi/bookstore/dto/BookDto.java
+++ src/main/java/yablunovskyi/bookstore/dto/BookDto.java
@@ -1,16 +1,14 @@
 package yablunovskyi.bookstore.dto;
 
 import java.math.BigDecimal;
-import lombok.Data;
-
 
 public record BookDto(
-    Long id,
-    String title,
-    String author,
-    String isbn,
-    BigDecimal price,
-    String description,
-    String coverImage
+        Long id,
+        String title,
+        String author,
+        String isbn,
+        BigDecimal price,
+        String description,
+        String coverImage
 ) {
 }
--- src/main/java/yablunovskyi/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/yablunovskyi/bookstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,13 @@
 package yablunovskyi.bookstore.dto;
 
 import java.math.BigDecimal;
-import lombok.Data;
 
 public record CreateBookRequestDto(
-    String title,
-   String author,
-    String isbn,
-   BigDecimal price,
-   String description,
-    String coverImage
+        String title,
+        String author,
+        String isbn,
+        BigDecimal price,
+        String description,
+        String coverImage
 ) {
 }
