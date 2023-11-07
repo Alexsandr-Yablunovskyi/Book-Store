379b3be982080a68b6ebeabd213653f459aa7279
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-11-07 17:26:08 +0200

    Category mapper is added

---
+++ src/main/java/yablunovskyi/bookstore/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package yablunovskyi.bookstore.mapper;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+import yablunovskyi.bookstore.dto.category.CategoryRequestDto;
+import yablunovskyi.bookstore.dto.category.CategoryResponseDto;
+import yablunovskyi.bookstore.model.Category;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
+public interface CategoryMapper {
+    CategoryResponseDto toDto(Category category);
+    
+    Category toCategory(CategoryRequestDto requestDto0);
+}
