3880f53676ec41403aed65d8e825fb2caa56ea0a
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-11-28 16:49:43 +0200

    Add ShoppingCart and CartItem entities

---
+++ src/main/java/yablunovskyi/bookstore/model/ShoppingCart.java
@@ -0,0 +1,34 @@
+package yablunovskyi.bookstore.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "shopping_Cart")
+@Getter
+@Setter
+public class ShoppingCart {
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id", nullable = false)
+    private User user;
+    
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+    
+}
