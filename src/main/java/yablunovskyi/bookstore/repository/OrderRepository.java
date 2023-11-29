b16f88e997540f02e9c43520218d010aaa36f2c7
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-11-29 15:55:54 +0200

    Add repo and service interface

---
+++ src/main/java/yablunovskyi/bookstore/repository/OrderRepository.java
@@ -0,0 +1,18 @@
+package yablunovskyi.bookstore.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import yablunovskyi.bookstore.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @EntityGraph(attributePaths = {"orderItems", "user", "orderItems.book"})
+    List<Order> findByUserId(Long id, Pageable pageable);
+    
+    @EntityGraph(attributePaths = "orderItems")
+    Optional<Order> findByUserIdAndId(Long userId, Long id);
+}
