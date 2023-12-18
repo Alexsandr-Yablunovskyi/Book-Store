e136ccfcb690c6164f88ff5c9601441cb2007057
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-12-18 16:03:29 +0200

    [API] Added OpenApiConfig

---
+++ src/main/java/yablunovskyi/bookstore/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package yablunovskyi.bookstore.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI cumtomOpenApi() {
+        Info info = new Info()
+                .title("Online Book Store")
+                .version("1.0")
+                .description("""
+                        This API represents endpoint to manage books, add them to shopping cart
+                        and place orders.
+                        """);
+        return new OpenAPI()
+                .info(info)
+                .components(new Components().addSecuritySchemes("BearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+    }
+}
