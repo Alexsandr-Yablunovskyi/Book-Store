1774d740ac4fca35be85583594f392066ad98a55
Oleksandr Yablunovskyi <briva.study@gmail.com>
2024-01-24 17:58:18 +0200

    [FIX] Packages name changed. BookStoreApplicationTests class deleted

--- src/test/java/yablunovskyi/bookstore/service/BookServiceTests.java
+++ src/test/java/yablunovskyi/bookstore/service/BookServiceTests.java
@@ -1,4 +1,4 @@
-package yablunovskyi.bookstore.booktests;
+package yablunovskyi.bookstore.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
--- src/test/java/yablunovskyi/bookstore/service/CategoryServiceTests.java
+++ src/test/java/yablunovskyi/bookstore/service/CategoryServiceTests.java
@@ -1,4 +1,4 @@
-package yablunovskyi.bookstore.categorytests;
+package yablunovskyi.bookstore.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
