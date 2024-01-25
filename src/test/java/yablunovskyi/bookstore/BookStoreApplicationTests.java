3a9bf5e450d0c2d4f1da9b5a08317391172447b4
Oleksandr Yablunovskyi <briva.study@gmail.com>
2024-01-25 17:08:15 +0200

    [FIX] Add BookStoreApplicationTests

---
+++ src/test/java/yablunovskyi/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,11 @@
+package yablunovskyi.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookStoreApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
